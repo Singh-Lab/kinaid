ef9b6b14546e7a70a6441234502690fa2f058e84
Javed Aman <javisaman@gmail.com>
2024-09-27 15:21:02 -0400

    changed port back to 8050

--- dashboard.py
+++ dashboard.py
@@ -1688,4 +1688,4 @@ def update_peptide_scatter_figure(kinase_names, session, started) :
   return fig, False
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port='8060', debug=True)
+  app.run(host='0.0.0.0', port='8050', debug=True)
